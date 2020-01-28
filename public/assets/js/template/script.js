9fce4f030f799a40382cf4f2d43cb6333626aecd
Alaercio2000 <alaercio.vieira@outlook.com>
2020-01-27 21:07:40 -0300

    Adicionando modal dos login no template e limpando o código do js do template

--- public/assets/js/template/script.js
+++ public/assets/js/template/script.js
@@ -1,23 +1,45 @@
 window.onscroll = function () { menuScroll()}
 
-var elementosNav = document.querySelectorAll("#textMenu");
+let elementosNav = document.querySelectorAll(".navItem");
 
 function menuScroll() {
   if (document.documentElement.scrollTop > 50) {
     document.getElementById("menuHeader").setAttribute("style","background-color:#FFDD8C");
-    document.getElementById("iconeMenu").classList.add("text-dark");
-    document.getElementById("textMenu1").classList.add("text-dark");
-    document.getElementById("textMenu2").classList.add("text-dark");
-    document.getElementById("textMenu3").classList.add("text-dark");
-    document.getElementById("textMenu4").classList.add("text-dark");
-    document.getElementById("textMenu5").classList.add("text-dark");
+    elementosNav.forEach(function(elem) {
+        elem.classList.add('text-dark');
+      });
   } else {
     document.getElementById("menuHeader").removeAttribute("style");
-    document.getElementById("iconeMenu").classList.remove("text-dark");
-    document.getElementById("textMenu1").classList.remove("text-dark");
-    document.getElementById("textMenu2").classList.remove("text-dark");
-    document.getElementById("textMenu3").classList.remove("text-dark");
-    document.getElementById("textMenu4").classList.remove("text-dark");
-    document.getElementById("textMenu5").classList.remove("text-dark");
+    elementosNav.forEach(function(elem) {
+        elem.classList.remove('text-dark');
+      });
   }
+}
+
+// Modal
+
+function showModalLogin(){
+    hideModalCadastro();
+    if (document.getElementById("modal-login").classList.contains("d-none")){
+        document.getElementById("modal-login").classList.remove("d-none");
+    }else{
+        document.getElementById("modal-login").classList.add("d-none");
+    }
+}
+
+function showModalCadastro(){
+    hideModalLogin();
+    if (document.getElementById("modal-cadastro").classList.contains("d-none")){
+        document.getElementById("modal-cadastro").classList.remove("d-none");
+    }else{
+        document.getElementById("modal-cadastro").classList.add("d-none");
+    }
+}
+
+function hideModalLogin(){
+    document.getElementById("modal-login").classList.add("d-none");
+}
+
+function hideModalCadastro(){
+    document.getElementById("modal-cadastro").classList.add("d-none");
 }
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -22,51 +22,43 @@
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
     <link rel="stylesheet" href="{{asset('assets/css/template/style.css')}}">
-
     @yield('css')
-
     <script defer src="{{asset('assets/js/template/script.js')}}"></script>
-
 </head>
 
 <body>
 
     <header id="menuHeader" class="fixed-top">
-        <div  class="container-fluid">
+        <div class="container-fluid">
             <div class="row flex-row-reverse flex-md-row">
-                <div class="col-6 col-pq-5 col-sm-8 col-md-2 pr-1">
-                <a class="col" href="{{route('home.index')}}">
-                    <img class="p-2 p-pq-1" height="90" src="{{asset('assets/img/template/logo.png')}}" alt="Logo">
+                <div class="col-5 col-sm-4 col-md-2">
+                    <a class="col" href="{{route('home.index')}}">
+                        <img class="p-2 p-pq-1" height="90" src="{{asset('assets/img/template/logo.png')}}" alt="Logo">
                     </a>
                 </div>
                 <div class="col">
                     <nav class="navbar navbar-expand-md">
                         <div>
-                            <button id="botaoMenu" class="navbar-toggler" type="button" data-toggle="collapse"
+                            <button id="botaoMenu" class="navbar-toggler ml-n3" type="button" data-toggle="collapse"
                                 data-target="#navBar" aria-controls="navBar" aria-expanded="false"
                                 aria-label="Toggle navigation">
-                                <i id="iconeMenu" class="material-icons">
+                                <i id="iconeMenu" class="material-icons navItem">
                                     menu
                                 </i>
                             </button>
                         </div>
-                        <div id="navBar" class="nav collapse navbar-collapse justify-content-end">
+                        <div id="navBar" class="nav collapse navbar-collapse justify-content-md-end">
                             <ul class="navbar-nav">
-                                <a id="textMenu1"
-                                    class="nav-link text-light font-weight-bold py-4 pr-3"
-                                    href="{{route('home.index')}}">Home &nbsp; |</a>
-                                <a id="textMenu2"
-                                    class="nav-link text-light font-weight-bold py-4 pr-3"
-                                    href="{{route('recipes.index')}}">Receitas &nbsp; |</a>
-                                <a id="textMenu3"
-                                    class="nav-link text-light font-weight-bold py-4 pr-5"
-                                    href="{{route('community.index')}}">Comunidade &nbsp; |</a>
-                                <a id="textMenu4"
-                                    class="nav-link text-light font-weight-bold pl-5 py-4"
-                                    href="#">Acesse</a>
-                                <a id="textMenu5"
-                                    class="nav-link text-light font-weight-bold py-4"
-                                    href="#">Registre-se</a>
+                                <a class="nav-link navItem text-light font-weight-bold py-3 py-md-4 pr-3"
+                                    href="{{route('home.index')}}">Home &nbsp;<span class="d-none d-md-inline">|</span></a>
+                                <a class="nav-link navItem text-light font-weight-bold py-3 py-md-4 pr-3"
+                                    href="{{route('recipes.index')}}">Receitas &nbsp;<span class="d-none d-md-inline">|</span></a>
+                                <a class="nav-link navItem text-light font-weight-bold py-3 py-md-4 pr-5"
+                                    href="{{route('community.index')}}">Comunidade &nbsp;<span class="d-none d-md-inline">|</span></a>
+                                <a class="nav-link navItem text-light font-weight-bold pl-lg-5 py-3 py-md-4"
+                                    onClick="showModalLogin()" href="javascript:void('')">Acesse</a>
+                                <a class="nav-link navItem text-light font-weight-bold py-3 py-md-4"
+                                    onClick="showModalCadastro()" href="javascript:void('')">Registre-se</a>
                             </ul>
                         </div>
                     </nav>
@@ -74,6 +66,104 @@
             </div>
         </div>
     </header>
+
+    <div class="modals container-fluid">
+        <div class="row justify-content-end">
+            <div id="modal-cadastro"
+                class="modal-cadastro text-center align-items-center col-12 col-md-5 col-lg-4 col-full-3 mr-3 d-none">
+                <form action="" method="POST">
+
+                    <div class="row d-flex flex-column my-3 align-items-center">
+
+                        <div class="close-modal" onClick="hideModalCadastro()">X</div>
+
+                        <h3>Cadastro</h3>
+
+                        <button type="submit" class="btn btn-light border my-2 mt-4 col-6 btn-social">
+                            <i class="fab fa-facebook-square"></i>
+                            Entrar com Facebook
+                        </button>
+
+                        <button type="submit" class="btn btn-light border my-2 mb-4 col-6 btn-social">
+                            <i class="fab fa-google"></i>
+                            Entrar com Google
+                        </button>
+
+                        <p>Ou cadastre-se usando seu e-mail</p>
+
+                        <div class="form-group col-10 my-2">
+                            <input type="text" class="form-control text-center" name="name" id="name"
+                                placeholder="Nome">
+                        </div>
+
+                        <div class="form-group col-10 my-2">
+                            <input type="email" class="form-control text-center" name="emailRegister" id="emailRegister"
+                                placeholder="E-mail">
+                        </div>
+
+                        <div class="form-group col-10 my-2">
+                            <input type="password" class="form-control text-center" name="passwordRegister" id="passwordRegister"
+                                placeholder="Senha">
+                        </div>
+
+                    </div>
+
+                    <button type="submit" class="btn btn-primary mb-1">Criar conta</button>
+
+                    <p class="mb-1"> Já tem uma conta? <a href="javascript:void('')" onClick="showModalLogin()">Entrar</a>
+                    </p>
+
+                    <p>Você é profissional? <a href="#">Clique aqui</a></p>
+
+                </form>
+            </div>
+        </div>
+        <div class="row justify-content-end">
+            <div id="modal-login"
+                class="modal-login text-center align-items-center col-12 col-md-5 col-lg-4 col-full-3 mr-3 d-none">
+                <form action="" method="POST">
+
+                    <div class="row d-flex flex-column my-3 align-items-center">
+
+                        <div class="close-modal" onClick="hideModalLogin()">X</div>
+
+                        <h3 class="mb-2">Entre em sua conta</h3>
+
+                        <h6>Não possui uma conta? <a href="javascript:void('')"
+                                onClick="showModalCadastro()">Registre-se</a></h6>
+
+                        <button type="submit" class="btn border my-2 mt-4 col-6">
+                            <i class="fab fa-facebook-square"></i>
+                            Entrar com Facebook
+                        </button>
+
+                        <button type="submit" class="btn border my-2 mb-4 col-6">
+                            <i class="fab fa-google"></i>
+                            Entrar com Google
+                        </button>
+
+                        <div class="form-group col-10 my-3">
+                            <input type="email" class="form-control text-center" name="emailLogin" id="emailLogin"
+                                placeholder="E-mail">
+                        </div>
+
+                        <div class="form-group col-10 my-3">
+                            <input type="password" class="form-control text-center" name="passwordLogin" id="passwordLogin"
+                                placeholder="Senha">
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary mb-2">Entrar</button>
+
+                    <p class="mb-1"><a href="">Esqueceu sua senha?</a></p>
+
+                    <p>Você é profissional? <a href="#">Clique aqui</a></p>
+
+                </form>
+            </div>
+        </div>
+    </div>
+
 
     @yield('content')
 
