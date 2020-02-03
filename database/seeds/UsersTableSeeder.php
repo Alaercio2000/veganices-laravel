31ee81319a0e7b93dbb6593bc796a555ab1d77c0
Alaercio2000 <alaercio.vieira@outlook.com>
2020-02-02 21:26:17 -0300

    Adicionando seeder de criação dos usuários padrões

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Alaercio',
+                'email' => 'alaercio@veganices.com.br',
+                'password' => Hash::make('1234'),
+                'avatar' => '49242568.jpg',
+                'created_at' => NOW(),
+                'updated_at' => NOW(),
+            ],
+            [
+                'name' => 'Ana',
+                'email' => 'ana@veganices.com.br',
+                'password' => Hash::make('1234'),
+                'avatar' => '11450997.jpg',
+                'created_at' => NOW(),
+                'updated_at' => NOW(),
+            ],
+            [
+                'name' => 'Edmar',
+                'email' => 'edmar@veganices.com.br',
+                'password' => Hash::make('1234'),
+                'avatar' => '53894735.jpg',
+                'created_at' => NOW(),
+                'updated_at' => NOW(),
+            ],
+            [
+                'name' => 'Meg',
+                'email' => 'meg@veganices.com.br',
+                'password' => Hash::make('1234'),
+                'avatar' => '55217950.jpg',
+                'created_at' => NOW(),
+                'updated_at' => NOW(),
+            ],
+        ]);
+    }
+}
