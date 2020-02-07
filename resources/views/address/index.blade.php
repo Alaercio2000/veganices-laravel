4cc7de783d2e5ac4562dd67b2c88dc571f4a324a
Alaercio2000 <alaercio.vieira@outlook.com>
2020-02-06 23:29:56 -0300

    Adicionando a view index e css e js para o crud de address

---
+++ resources/views/address/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.template')
+
+@section('title','Endereços do Usuário')
+
+@section('css')
+<link rel="stylesheet" href="{{asset('assets/css/address/style.css')}}">
+@endsection
+
+@section('content')
+
+
+
+@endsection
