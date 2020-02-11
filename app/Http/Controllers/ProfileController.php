60807903b94b12d79622f8a780634c2928d8b26a
Alaercio2000 <alaercio.vieira@outlook.com>
2020-02-10 23:22:28 -0300

    Adicionando o controller de profile e removendo o middleware do construct

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CommunityController extends Controller
+class ProfileController extends Controller
 {
     public function index(){
-        return view('community.index');
+        return view('profile.index');
     }
 }
