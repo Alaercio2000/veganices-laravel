aa8de921f5fee0e2e0fb952d5675d2253c9a41c4
Alaercio <alaercio.vieira@outlook.com>
2020-02-17 09:28:28 -0300

    arrumando o campo de select dos estados

--- resources/views/address/create.blade.php
+++ resources/views/address/create.blade.php
@@ -10,10 +10,6 @@
 <script src="{{asset('assets/js/address/script.js')}}"></script>
 @endsection
 
-@section('body')
-onload="selectedState('{{old('state')}}')"
-@endsection
-
 @section('content')
 
 <div class="container">
@@ -26,7 +22,8 @@ onload="selectedState('{{old('state')}}')"
                 @csrf
                 <div class="form-group row">
                     <label class="col-4 col-lg-3 pt-1" for="title">Titulo</label>
-                    <input id="title" name="title" type="text" class="form-control col @error('title') is-invalid @enderror" value="{{old('title')}}">
+                    <input id="title" name="title" type="text"
+                        class="form-control col @error('title') is-invalid @enderror" value="{{old('title')}}">
                     <div class="invalid-feedback offset-lg-3 offset-4">
                         {{$errors->first('title')}}
                     </div>
@@ -34,7 +31,8 @@ onload="selectedState('{{old('state')}}')"
 
                 <div class="form-group row">
                     <label class="col-4 col-lg-3 pt-1" for="zip_code">Cep</label>
-                    <input id="zip_code" name="zip_code" type="text" class="form-control col @error('zip_code') is-invalid @enderror" value="{{old('zip_code')}}">
+                    <input id="zip_code" name="zip_code" type="text"
+                        class="form-control col @error('zip_code') is-invalid @enderror" value="{{old('zip_code')}}">
                     <div class="invalid-feedback offset-lg-3 offset-4">
                         {{$errors->first('zip_code')}}
                     </div>
@@ -42,7 +40,8 @@ onload="selectedState('{{old('state')}}')"
 
                 <div class="form-group row">
                     <label class="col-4 col-lg-3 pt-1" for="street">Rua</label>
-                    <input id="street" name="street" type="text" class="form-control col @error('street') is-invalid @enderror" value="{{old('street')}}">
+                    <input id="street" name="street" type="text"
+                        class="form-control col @error('street') is-invalid @enderror" value="{{old('street')}}">
                     <div class="invalid-feedback offset-lg-3 offset-4">
                         {{$errors->first('street')}}
                     </div>
@@ -50,16 +49,18 @@ onload="selectedState('{{old('state')}}')"
 
                 <div class="form-group row">
                     <label class="col-4 col-lg-3 pt-1" for="neighborhood">Bairro</label>
-                    <input id="neighborhood" name="neighborhood" type="text" class="form-control col @error('neighborhood') is-invalid @enderror"
+                    <input id="neighborhood" name="neighborhood" type="text"
+                        class="form-control col @error('neighborhood') is-invalid @enderror"
                         value="{{old('neighborhood')}}">
-                        <div class="invalid-feedback offset-lg-3 offset-4">
-                            {{$errors->first('neighborhood')}}
-                        </div>
+                    <div class="invalid-feedback offset-lg-3 offset-4">
+                        {{$errors->first('neighborhood')}}
+                    </div>
                 </div>
 
                 <div class="form-group row">
                     <label class="col-4 col-lg-3 pt-1" for="">Número</label>
-                    <input id="number" name="number" type="text" class="form-control col @error('number') is-invalid @enderror" value="{{old('number')}}">
+                    <input id="number" name="number" type="text"
+                        class="form-control col @error('number') is-invalid @enderror" value="{{old('number')}}">
                     <div class="invalid-feedback offset-lg-3 offset-4">
                         {{$errors->first('number')}}
                     </div>
@@ -67,16 +68,18 @@ onload="selectedState('{{old('state')}}')"
 
                 <div class="form-group row">
                     <label class="col-4 col-lg-3 pt-1" for="complement">Complemento</label>
-                    <input id="complement" name="complement" type="text" class="form-control col @error('complement') is-invalid @enderror"
+                    <input id="complement" name="complement" type="text"
+                        class="form-control col @error('complement') is-invalid @enderror"
                         value="{{old('complement')}}">
-                        <div class="invalid-feedback offset-lg-3 offset-4">
-                            {{$errors->first('complement')}}
-                        </div>
+                    <div class="invalid-feedback offset-lg-3 offset-4">
+                        {{$errors->first('complement')}}
+                    </div>
                 </div>
 
                 <div class="form-group row">
                     <label class="col-4 col-lg-3 pt-1" for="">Cidade</label>
-                    <input id="county" name="county" type="text" class="form-control col @error('county') is-invalid @enderror" value="{{old('county')}}">
+                    <input id="county" name="county" type="text"
+                        class="form-control col @error('county') is-invalid @enderror" value="{{old('county')}}">
                     <div class="invalid-feedback offset-lg-3 offset-4">
                         {{$errors->first('county')}}
                     </div>
@@ -84,34 +87,11 @@ onload="selectedState('{{old('state')}}')"
                 <div class="form-group row">
                     <label class="col-4 col-lg-3 pt-1" for="state">Estado</label>
                     <select id="state" name="state" class="form-control col @error('state') is-invalid @enderror">
-                        <option value=""></option>
-                        <option value="AC">Acre</option>
-                        <option value="AL">Alagoas</option>
-                        <option value="AP">Amapá</option>
-                        <option value="AM">Amazonas</option>
-                        <option value="BA">Bahia</option>
-                        <option value="CE">Ceará</option>
-                        <option value="DF">Distrito Federal</option>
-                        <option value="ES">Espírito Santo</option>
-                        <option value="GO">Goiás</option>
-                        <option value="MA">Maranhão</option>
-                        <option value="MT">Mato Grosso</option>
-                        <option value="MS">Mato Grosso do Sul</option>
-                        <option value="MG">Minas Gerais</option>
-                        <option value="PA">Pará</option>
-                        <option value="PB">Paraíba</option>
-                        <option value="PR">Paraná</option>
-                        <option value="PE">Pernambuco</option>
-                        <option value="PI">Piauí</option>
-                        <option value="RJ">Rio de Janeiro</option>
-                        <option value="RN">Rio Grande do Norte</option>
-                        <option value="RS">Rio Grande do Sul</option>
-                        <option value="RO">Rondônia</option>
-                        <option value="RR">Roraima</option>
-                        <option value="SC">Santa Catarina</option>
-                        <option value="SP">São Paulo</option>
-                        <option value="SE">Sergipe</option>
-                        <option value="TO">Tocantins</option>
+                        <option value="">Selecione</option>
+                        @foreach ($states as $state)
+                        <option {{(old('state') == $state->id)?'selected':''}} value="{{$state->id}}">{{$state->name}}
+                        </option>
+                        @endforeach
                     </select>
                     <div class="invalid-feedback offset-lg-3 offset-4">
                         {{$errors->first('state')}}
--- resources/views/address/edit.blade.php
+++ resources/views/address/edit.blade.php
@@ -10,24 +10,32 @@
 <script src="{{asset('assets/js/address/script.js')}}"></script>
 @endsection
 
-@section('body')
-onload="selectedState('{{$address->state}}')"
-@endsection
-
 @section('content')
 
 <div class="container">
     <main id="corpo">
-        <h1 class="text-center py-3">Adicionar endereço</h1>
+
+        @if (session('success'))
+        <div class="alert alert-success col-md-8 offset-md-2 mt-5 alert-dismissible fade show">
+            {{session('success')}}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        @endif
+
+        <h1 class="text-center py-3">Editar endereço</h1>
 
 
         <div class="card border-0">
-            <form action="{{route('address.update',['address' => $address->id])}}" method="post" class="col-md-8 offset-md-2 form-horizontal">
+            <form action="{{route('address.update',['address' => $address->id])}}" method="post"
+                class="col-md-8 offset-md-2 form-horizontal">
                 @csrf
                 @method('PUT')
                 <div class="form-group row">
                     <label class="col-4 col-lg-3 pt-1" for="title">Titulo</label>
-                    <input id="title" name="title" type="text" class="form-control col @error('title') is-invalid @enderror" value="{{$address->title}}">
+                    <input id="title" name="title" type="text"
+                        class="form-control col @error('title') is-invalid @enderror" value="{{$address->title}}">
                     <div class="invalid-feedback offset-lg-3 offset-4">
                         {{$errors->first('title')}}
                     </div>
@@ -35,7 +43,8 @@ onload="selectedState('{{$address->state}}')"
 
                 <div class="form-group row">
                     <label class="col-4 col-lg-3 pt-1" for="zip_code">Cep</label>
-                    <input id="zip_code" name="zip_code" type="text" class="form-control col @error('zip_code') is-invalid @enderror" value="{{$address->zip_code}}">
+                    <input id="zip_code" name="zip_code" type="text"
+                        class="form-control col @error('zip_code') is-invalid @enderror" value="{{$address->zip_code}}">
                     <div class="invalid-feedback offset-lg-3 offset-4">
                         {{$errors->first('zip_code')}}
                     </div>
@@ -43,7 +52,8 @@ onload="selectedState('{{$address->state}}')"
 
                 <div class="form-group row">
                     <label class="col-4 col-lg-3 pt-1" for="street">Rua</label>
-                    <input id="street" name="street" type="text" class="form-control col @error('street') is-invalid @enderror" value="{{$address->street}}">
+                    <input id="street" name="street" type="text"
+                        class="form-control col @error('street') is-invalid @enderror" value="{{$address->street}}">
                     <div class="invalid-feedback offset-lg-3 offset-4">
                         {{$errors->first('street')}}
                     </div>
@@ -51,16 +61,18 @@ onload="selectedState('{{$address->state}}')"
 
                 <div class="form-group row">
                     <label class="col-4 col-lg-3 pt-1" for="neighborhood">Bairro</label>
-                    <input id="neighborhood" name="neighborhood" type="text" class="form-control col @error('neighborhood') is-invalid @enderror"
+                    <input id="neighborhood" name="neighborhood" type="text"
+                        class="form-control col @error('neighborhood') is-invalid @enderror"
                         value="{{$address->neighborhood}}">
-                        <div class="invalid-feedback offset-lg-3 offset-4">
-                            {{$errors->first('neighborhood')}}
-                        </div>
+                    <div class="invalid-feedback offset-lg-3 offset-4">
+                        {{$errors->first('neighborhood')}}
+                    </div>
                 </div>
 
                 <div class="form-group row">
                     <label class="col-4 col-lg-3 pt-1" for="">Número</label>
-                    <input id="number" name="number" type="text" class="form-control col @error('number') is-invalid @enderror" value="{{$address->number}}">
+                    <input id="number" name="number" type="text"
+                        class="form-control col @error('number') is-invalid @enderror" value="{{$address->number}}">
                     <div class="invalid-feedback offset-lg-3 offset-4">
                         {{$errors->first('number')}}
                     </div>
@@ -68,16 +80,18 @@ onload="selectedState('{{$address->state}}')"
 
                 <div class="form-group row">
                     <label class="col-4 col-lg-3 pt-1" for="complement">Complemento</label>
-                    <input id="complement" name="complement" type="text" class="form-control col @error('complement') is-invalid @enderror"
+                    <input id="complement" name="complement" type="text"
+                        class="form-control col @error('complement') is-invalid @enderror"
                         value="{{$address->complement}}">
-                        <div class="invalid-feedback offset-lg-3 offset-4">
-                            {{$errors->first('complement')}}
-                        </div>
+                    <div class="invalid-feedback offset-lg-3 offset-4">
+                        {{$errors->first('complement')}}
+                    </div>
                 </div>
 
                 <div class="form-group row">
                     <label class="col-4 col-lg-3 pt-1" for="">Cidade</label>
-                    <input id="county" name="county" type="text" class="form-control col @error('county') is-invalid @enderror" value="{{$address->county}}">
+                    <input id="county" name="county" type="text"
+                        class="form-control col @error('county') is-invalid @enderror" value="{{$address->county}}">
                     <div class="invalid-feedback offset-lg-3 offset-4">
                         {{$errors->first('county')}}
                     </div>
@@ -85,34 +99,12 @@ onload="selectedState('{{$address->state}}')"
                 <div class="form-group row">
                     <label class="col-4 col-lg-3 pt-1" for="state">Estado</label>
                     <select id="state" name="state" class="form-control col @error('state') is-invalid @enderror">
-                        <option value=""></option>
-                        <option value="AC">Acre</option>
-                        <option value="AL">Alagoas</option>
-                        <option value="AP">Amapá</option>
-                        <option value="AM">Amazonas</option>
-                        <option value="BA">Bahia</option>
-                        <option value="CE">Ceará</option>
-                        <option value="DF">Distrito Federal</option>
-                        <option value="ES">Espírito Santo</option>
-                        <option value="GO">Goiás</option>
-                        <option value="MA">Maranhão</option>
-                        <option value="MT">Mato Grosso</option>
-                        <option value="MS">Mato Grosso do Sul</option>
-                        <option value="MG">Minas Gerais</option>
-                        <option value="PA">Pará</option>
-                        <option value="PB">Paraíba</option>
-                        <option value="PR">Paraná</option>
-                        <option value="PE">Pernambuco</option>
-                        <option value="PI">Piauí</option>
-                        <option value="RJ">Rio de Janeiro</option>
-                        <option value="RN">Rio Grande do Norte</option>
-                        <option value="RS">Rio Grande do Sul</option>
-                        <option value="RO">Rondônia</option>
-                        <option value="RR">Roraima</option>
-                        <option value="SC">Santa Catarina</option>
-                        <option value="SP">São Paulo</option>
-                        <option value="SE">Sergipe</option>
-                        <option value="TO">Tocantins</option>
+                        <option value="">Selecione</option>
+                        @foreach ($states as $state)
+                        <option {{($address->state_id == $state->id)?'selected':''}} value="{{$state->id}}">
+                            {{$state->name}}
+                        </option>
+                        @endforeach
                     </select>
                     <div class="invalid-feedback offset-lg-3 offset-4">
                         {{$errors->first('state')}}
@@ -121,7 +113,7 @@ onload="selectedState('{{$address->state}}')"
 
                 <div class="form-group row">
                     <label class="col-4 col-lg-3" for=""></label>
-                    <input type="submit" value="Adicionar" class="btn btn-success col my-3 w-100">
+                    <input type="submit" value="Salvar" class="btn btn-success col my-3 w-100">
                 </div>
             </form>
         </div>
