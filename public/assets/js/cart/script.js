8cd791bee4d3b25a8722ea5f1af3ac34baa765d9
Alaercio2000 <alaercio.vieira@outlook.com>
2020-02-24 01:17:35 -0300

    Arquivo de js para calcular o frete

--- public/assets/js/cart/script.js
+++ public/assets/js/cart/script.js
@@ -1,1 +1,22 @@
 $('#zipCodeSource').mask('00000-000');
+
+function calcShipping() {
+    var zip_code = $('#zipCodeSource');
+    zip_code = zip_code.val().replace(/\D/g, '');
+
+    if (zip_code != '') {
+        var validateZipCode = /^[0-9]{8}$/;
+
+        if (validateZipCode.test(zip_code)) {
+            $.getJSON("127.0.0.1/api/calculate-shipping/" + zip_code, function (data) {
+                if (!("erro" in data)) {
+                    console.log(data);
+                }else{
+                    console.log('deu erro');
+                }
+            });
+        } else {
+            alert('Cep inválido');
+        }
+    }
+}
