734e69ce30647cfde21158dcd5526337e489eb5c
Alaercio2000 <alaercio.vieira@outlook.com>
2020-02-24 18:04:26 -0300

    Calculando o frete e passando o valor do resumo do pedido

--- public/assets/js/cart/script.js
+++ public/assets/js/cart/script.js
@@ -8,15 +8,40 @@ function calcShipping() {
         var validateZipCode = /^[0-9]{8}$/;
 
         if (validateZipCode.test(zip_code)) {
-            $.getJSON("127.0.0.1/api/calculate-shipping/" + zip_code, function (data) {
-                if (!("erro" in data)) {
-                    console.log(data);
+            $.getJSON("https://viacep.com.br/ws/"+ zip_code +"/json/?callback=?", function(cepValid) {
+                if (!("erro" in cepValid)) {
+
+                    var req = new Request('/api/calculate-shipping/' + zip_code);
+
+                    fetch(req).then(function(response) {
+                        response.json().then(function(data){
+                            localStorage.setItem('answerShipping','O valor do frete é ' + data.Valor + ' em até '+ data.PrazoEntrega + ' úteis');
+                            localStorage.setItem('shipping', data.Valor);
+                            localStorage.setItem('termShipping' , data.PrazoEntrega);
+
+                            $('#valueAllProducts').html((parseFloat(localStorage.getItem('shipping').replace(',','.')) + parseFloat(valueAllProducts)).toFixed(2));
+                            $('#answerShipping').html(localStorage.getItem('answerShipping'));
+                            $('#valueShipping').html(localStorage.getItem('shipping'));
+                            $('#zipCodeSource').removeClass('is-invalid');
+                            $('#zipCodeSource').addClass('is-valid');
+                        });
+                    });
                 }else{
-                    console.log('deu erro');
+                    $('#zipCodeSource').addClass('is-invalid');
+                    $('#invalidZipCode').html('Cep não encontrado')
                 }
             });
         } else {
-            alert('Cep inválido');
+            $('#zipCodeSource').addClass('is-invalid');
+            $('#invalidZipCode').html('Digite um cep válido')
         }
+    }else {
+        $('#zipCodeSource').addClass('is-invalid');
+        $('#invalidZipCode').html('Digite um cep');
     }
 }
+
+$('#answerShipping').html(localStorage.getItem('answerShipping'));
+$('#valueShipping').html(localStorage.getItem('shipping'));
+
+$('#valueAllProducts').html((parseFloat(localStorage.getItem('shipping').replace(',','.')) + parseFloat(valueAllProducts)).toFixed(2));
