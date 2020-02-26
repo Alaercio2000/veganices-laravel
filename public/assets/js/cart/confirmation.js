61be644e68a5b613183b7eb9b9586f1cce39af5e
Alaercio2000 <alaercio.vieira@outlook.com>
2020-02-26 18:18:25 -0300

    Acões com o js no carrinho

--- public/assets/js/cart/confirmation.js
+++ public/assets/js/cart/confirmation.js
@@ -5,25 +5,35 @@ var req = new Request('/api/calculate-shipping/' + zip_code);
 fetch(req).then(function (response) {
     response.json().then(function (data) {
         $('#valueShipping').html(data.Valor);
-        $('#answerShipping').html('O valor do frete é ' + data.Valor + ' em até '+ data.PrazoEntrega + ' úteis');
-        $('#valueAllProducts').html(((parseFloat(data.Valor.replace(',','.')) + parseFloat(valueAllProducts)).toFixed(2)).replace('.',','));
-        $('#valueBillet').html(((parseFloat(data.Valor.replace(',','.')) + parseFloat(valueAllProducts)).toFixed(2)).replace('.',','));
+        $('#answerShipping').html('O valor do frete é ' + data.Valor + ' em até ' + data.PrazoEntrega + ' úteis');
+        $('#valueAllProducts').html(((parseFloat(data.Valor.replace(',', '.')) + parseFloat(valueAllProducts)).toFixed(2)).replace('.', ','));
+        $('#valueBillet').html(((parseFloat(data.Valor.replace(',', '.')) + parseFloat(valueAllProducts)).toFixed(2)).replace('.', ','));
 
-        localStorage.setItem('priceAll',((parseFloat(data.Valor.replace(',','.')) + parseFloat(valueAllProducts)).toFixed(2)).replace('.',','));
+        localStorage.setItem('priceAll', ((parseFloat(data.Valor.replace(',', '.')) + parseFloat(valueAllProducts)).toFixed(2)).replace('.', ','));
     });
 });
 
-function showCard(){
+function showCard() {
     if ($('#formCard').hasClass('d-none')) {
+
         $('#formCard').removeClass('d-none');
         $('#formBillet').addClass('d-none');
+        $('#iconCard').css('color', '#1b6de8');
+        $('#iconBillet').css('color', '#ddd');
+
+        $('#rodape').removeClass('fixed-bottom');
     }
 }
 
-function showBillet(){
+function showBillet() {
+
     if ($('#formBillet').hasClass('d-none')) {
+
         $('#formBillet').removeClass('d-none');
         $('#formCard').addClass('d-none');
+        $('#iconCard').css('color','#ddd');
+        $('#iconBillet').css('color', '#1b6de8');
+
     }
 }
 
