d726ce1d69f4821efcd8728d19f1bbf7f8e35062
Alaercio <alaercio.vieira@outlook.com>
2020-02-27 10:06:05 -0300

    limpando o código e deixando em grupos

--- routes/web.php
+++ routes/web.php
@@ -11,62 +11,90 @@
 |
 */
 
+//Erro de página não encontrada
 Route::fallback(function () {
     return view("erro404");
 });
 
-Route::get('/', 'HomeController@index')->name('home.index');
 
-
-Route::resource('/recipes', 'RecipesController')->middleware('auth')->middleware('can:is-provider');
-
-Route::get('user/recipes' , 'RecipesUsersController@index')->name('user.recipes');
-Route::get('user/recipes/show/{id}' , 'RecipesUsersController@show')->name('user.recipe.show');
-
-Route::get('/community', 'CommunityController@index')->name('community.index');
-
+// Registro de usuário e fornecedores
 Route::prefix('register')->group(function () {
 
     Route::get('/', 'Auth\RegisterUserController@index')->name('register');
     Route::post('/', 'Auth\RegisterUserController@register');
 
     Route::get('provider', 'Auth\RegisterProviderController@index')->name('register.provider');
-    Route::post('provider','Auth\RegisterProviderController@register');
+    Route::post('provider', 'Auth\RegisterProviderController@register');
 });
+//
+
+
+// Rotas permitidas para usuários não logados
+Route::get('/', 'HomeController@index')->name('home.index');
+
+Route::get('user/recipes', 'RecipesUsersController@index')->name('user.recipes');
+Route::get('user/recipes/show/{id}', 'RecipesUsersController@show')->name('user.recipe.show');
+
+Route::get('/community', 'CommunityController@index')->name('community.index');
 
 Route::get('/login', 'Auth\LoginController@index')->name('login');
 Route::post('/login', 'Auth\LoginController@authenticade');
-
-Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
-
-Route::resource('/address','AddressController')->middleware('auth')->middleware('can:is-user');
+//
 
 
-Route::prefix('profile')->group(function (){
+// Apenas usuários logados
+Route::middleware('auth')->group(function () {
 
-    Route::get('/','ProfileController@index')->name('profile')->middleware('can:is-user');
-    Route::get('/edit' ,'ProfileController@edit')->name('profile.edit')->middleware('can:is-user');
-    Route::put('/edit' ,'ProfileController@update')->name('profile.update')->middleware('can:is-user');
+    // Apenas usuários normais
+    Route::middleware('can:is-user')->group(function () {
 
-    Route::prefix('provider')->group(function(){
+        Route::prefix('cart')->group(function () {
 
-        Route::get('/','ProfileProviderController@index')->name('profile.provider')->middleware('can:is-provider');
-        Route::get('/edit','ProfileProviderController@edit')->name('profile.provider.edit')->middleware('can:is-provider');
-        Route::put('/edit' ,'ProfileProviderController@update')->name('profile.provider.update')->middleware('can:is-provider');
+            Route::get('/', 'CartsController@index')->name('cart.index');
+            Route::get('confirmation', 'CartsController@confirmation')->name('cart.confirmation');
+            Route::put('confirmation/alter-address', 'CartsController@alterAddress')->name('cart.alter.address');
+            Route::post('create/{recipe}', 'CartsController@store')->name('cart.store');
+            Route::put('update/quantity/{id}', 'CartsController@updateQuantity')->name('cart.update.quantity');
+            Route::delete('destroy/{recipe}', 'CartsController@destroyRecipe')->name('cart.destroy.recipe');
+            Route::delete('destroy/cart/{id}', 'CartsController@destroy')->name('cart.destroy');
+
+        });
+
+        Route::prefix('profile')->group(function () {
+
+            Route::get('/', 'ProfileController@index')->name('profile');
+            Route::get('/edit', 'ProfileController@edit')->name('profile.edit');
+            Route::put('/edit', 'ProfileController@update')->name('profile.update');
+
+        });
+
+        Route::resource('/address', 'AddressController');
 
     });
+    //
 
-    Route::put('/upload/{route}', 'UploadController@uploadImageProfile')->name('upload')->middleware('auth');
-    Route::delete('/delImage/{id}/{route}','UploadController@deleteImage')->name('del.image')->middleware('auth');
+    // Ações para ambos usuários
+    Route::put('profile/upload/{route}', 'UploadController@uploadImageProfile')->name('upload');
+    Route::delete('profile/delImage/{id}/{route}', 'UploadController@deleteImage')->name('del.image');
+    Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
+    //
+
+
+
+    //Apenas fornecedores
+    Route::middleware('can:is-provider')->group(function () {
+
+        Route::resource('/recipes', 'RecipesController')->middleware('can:is-provider');
+
+
+        Route::prefix('profile/provider')->group(function () {
+
+            Route::get('/', 'ProfileProviderController@index')->name('profile.provider')->middleware('can:is-provider');
+            Route::get('/edit', 'ProfileProviderController@edit')->name('profile.provider.edit')->middleware('can:is-provider');
+            Route::put('/edit', 'ProfileProviderController@update')->name('profile.provider.update')->middleware('can:is-provider');
+
+        });
+    });
+    //
 });
-
-Route::prefix('cart')->group(function (){
-
-    Route::get('/','CartsController@index')->name('cart.index')->middleware('can:is-user');
-    Route::get('confirmation' , 'CartsController@confirmation')->name('cart.confirmation')->middleware('can:is-user');
-    Route::put('confirmation/alter-address' , 'CartsController@alterAddress')->name('cart.alter.address')->middleware('can:is-user');
-    Route::post('create/{recipe}', 'CartsController@store')->name('cart.store')->middleware('can:is-user');
-    Route::put('update/quantity/{id}','CartsController@updateQuantity')->name('cart.update.quantity')->middleware('can:is-user');
-    Route::delete('destroy/{recipe}' , 'CartsController@destroyRecipe')->name('cart.destroy.recipe')->middleware('can:is-user');
-    Route::delete('destroy/cart/{id}' , 'CartsController@destroy')->name('cart.destroy')->middleware('can:is-user');
-});
+//
