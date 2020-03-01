d9348a088324ae9272439d99e589e9b5d36ba521
megmiyata <megmiyata@hotmail.com>
2020-03-01 16:57:23 -0300

    criação das migrations da comunidade

--- app/Http/Controllers/CommunityController.php
+++ app/Http/Controllers/CommunityController.php
@@ -2,12 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Community;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class CommunityController extends Controller
 {
-    public function index(){
-        return view('community.index');
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Community  $community
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Community $community)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Community  $community
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Community $community)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Community  $community
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Community $community)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Community  $community
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Community $community)
+    {
+        //
     }
 }
